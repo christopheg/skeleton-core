768a068be72a96a98e035ec7d72726cd72993c1c
Gerry Demaret <gerry@tigron.be>
2015-08-14 16:11:26 +0200

    Support multiple include paths

--- lib/Skeleton/Core/Autoloader.php
+++ lib/Skeleton/Core/Autoloader.php
@@ -16,33 +16,22 @@ class Autoloader {
 	/**
 	 * @var string $file_extension
 	 */
-	 private $file_extension = '.php';
+	private $file_extension = '.php';
 
 	/**
 	 * @var string $namespace
 	 */
-	private $namespace;
+	private $namespace = null;
 
 	/**
-	 * @var string $include_path
+	 * @var string $include_paths
 	 */
-	private $include_path;
+	private $include_paths = [];
 
 	/**
 	 * @var string $namespace_separator
 	 */
 	private $namespace_separator = '\\';
-
-	/**
-	 * Creates a new Autoloader that loads classes of the specified namespace.
-	 *
-	 * @param string $namespace The namespace to use.
-	 * @param string $include_path The path to search for includes
-	 */
-	public function __construct($namespace = null, $include_path = null) {
-		$this->namespace = $namespace;
-		$this->include_path = $include_path;
-	}
 
 	/**
 	 * Sets the namespace separator used by classes in the namespace of this
@@ -60,28 +49,27 @@ class Autoloader {
 	 *
 	 * @return void
 	 */
-	public function set_namespace_separator() {
+	public function get_namespace_separator() {
 		return $this->namespace_separator;
 	}
 
 	/**
-	 * Sets the base include path for all class files in the namespace of this
+	 * Adds an include path for all class files in the namespace of this
 	 * class loader.
 	 *
 	 * @param string $include_path
 	 */
-	public function set_include_path($include_path) {
-		$this->include_path = $include_path;
+	public function add_include_path($include_path) {
+		$this->include_paths[] = $include_path;
 	}
 
 	/**
-	 * Gets the base include path for all class files in the namespace of this
-	 * class loader.
+	 * Gets the loaded include paths.
 	 *
 	 * @return string $include_path
 	 */
-	public function get_include_path() {
-		return $this->include_path;
+	public function get_include_paths() {
+		return $this->include_paths;
 	}
 
 	/**
@@ -123,19 +111,14 @@ class Autoloader {
 	 * @return void
 	 */
 	public function load_class($class_name) {
-		if (null === $this->namespace || $this->namespace.$this->namespace_separator === substr($class_name, 0, strlen($this->namespace.$this->namespace_separator))) {
-			$file_name = '';
-			$namespace = '';
+		$file_path = str_replace(' ', '/', ucwords(str_replace('_', ' ', $class_name))) . '.php';
 
-			if (false !== ($last_namespace_position = strripos($class_name, $this->namespace_separator))) {
-				$namespace = substr($class_name, 0, $last_namespace_position);
-				$class_name = substr($class_name, $last_namespace_position + 1);
-				$file_name = str_replace($this->namespace_separator, DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+		foreach ($this->include_paths as $include_path) {
+			$path = $include_path . '/' . $file_path;
+
+			if (file_exists($path)) {
+				require_once $path;
 			}
-
-			$file_name .= str_replace('_', DIRECTORY_SEPARATOR, $class_name) . $this->file_extension;
-
-			require ($this->include_path !== null ? $this->include_path . DIRECTORY_SEPARATOR : '') . $file_name;
 		}
 	}
 }
