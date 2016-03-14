12c0e1ca8f90f7ce7012ecfd5540eb86f05da413
Christophe Gosiau <christophe.gosiau@tigron.be>
2016-03-14 17:03:44 +0100

    performance improvement: cache skeleton packages

--- lib/Skeleton/Core/Package.php
+++ lib/Skeleton/Core/Package.php
@@ -43,40 +43,51 @@ class Package {
 	public $asset_path = null;
 
 	/**
+	 * Package cache
+	 *
+	 * @access private
+	 * @var array $package_cache
+	 */
+	private static $package_cache = null;
+
+	/**
 	 * Get all
 	 *
 	 * @access public
 	 * @return array $packages
 	 */
 	public static function get_all() {
-		/**
-		 * Search for other Skeleton packages installed
-		 */
-		$composer_dir = realpath(__DIR__ . '/../../../../../');
-		$installed = file_get_contents($composer_dir . '/composer/installed.json');
-		$installed = json_decode($installed);
+		if (self::$package_cache === null) {
+			/**
+			 * Search for other Skeleton packages installed
+			 */
+			$composer_dir = realpath(__DIR__ . '/../../../../../');
+			$installed = file_get_contents($composer_dir . '/composer/installed.json');
+			$installed = json_decode($installed);
 
-		$packages = [];
-		foreach ($installed as $install) {
-			$package = $install->name;
-			list($vendor, $name) = explode('/', $package);
-			if ($vendor != 'tigron') {
-				continue;
+			$packages = [];
+			foreach ($installed as $install) {
+				$package = $install->name;
+				list($vendor, $name) = explode('/', $package);
+				if ($vendor != 'tigron') {
+					continue;
+				}
+				if (strpos($name, 'skeleton-package') !== 0) {
+					continue;
+				}
+
+				$package = new self();
+				$package->name = $name;
+				$package->path = $composer_dir . '/tigron/' . $name;
+				$package->template_path = $composer_dir . '/tigron/' . $name . '/template';
+				$package->asset_path = $composer_dir . '/tigron/' . $name . '/media';
+				$package->migration_path = $composer_dir . '/tigron/' . $name . '/migration';
+
+				$packages[] = $package;
 			}
-			if (strpos($name, 'skeleton-package') !== 0) {
-				continue;
-			}
-
-			$package = new self();
-			$package->name = $name;
-			$package->path = $composer_dir . '/tigron/' . $name;
-			$package->template_path = $composer_dir . '/tigron/' . $name . '/template';
-			$package->asset_path = $composer_dir . '/tigron/' . $name . '/media';
-			$package->migration_path = $composer_dir . '/tigron/' . $name . '/migration';
-
-			$packages[] = $package;
+			self::$package_cache = $packages;
 		}
-		return $packages;
+		return self::$package_cache;
 	}
 
 }
