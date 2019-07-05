049a0de4181e7113b58a65fca61aeaa3c375aaa9
Christophe Gosiau <christophe@tigron.be>
2019-07-05 16:17:22 +0200

    new features: - events - update of readme
    This update removes the 'Hook'. Please be aware that this could break
    your existing implementation.

---
+++ lib/Skeleton/Core/Event.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Event
+ *
+ * @author Gerry Demaret <gerry@tigron.be>
+ * @author Christophe Gosiau <christophe@tigron.be>
+ * @author David Vandemaele <david@tigron.be>
+ */
+
+namespace Skeleton\Core;
+
+class Event {
+
+}
